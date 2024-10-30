f1f53bfa493ef1456ea3151ce7859d66b2038a2e
dingdangmaoup <dingdangmaoup@gmail.com>
2024-10-30 08:13:59 +0800

    refactor: Remove unused imports and clean up code

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,7 @@
 import TitleBar from "@/components/TitleBar";
 import useIsClient from "@/hooks/useIsClient";
 import { useAsyncEffect } from "ahooks";
-import Image from "next/image";
 import { platform } from '@tauri-apps/plugin-os';
-import { Menu } from "@/components/music/menu";
-import { Sidebar } from "@/components/music/sidebar";
-import { playlists } from "@/components/music/data/playlists";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { listenNowAlbums, madeForYouAlbums } from "@/components/music/data/albums";
-import { AlbumArtwork } from "@/components/music/album-artwork";
-import { PodcastEmptyPlaceholder } from "@/components/music/podcast-empty-placeholder";
-import Icon from "@/components/Icon";
 import MusicPage from "@/components/music/page";
 let currentPlatform = 'unknown';
 export default function Home() {
--- store/index.ts
+++ store/index.ts
@@ -1,5 +1,4 @@
 import {atom} from 'jotai';
 
 const countAtom = atom(0);
-
 export {countAtom};
